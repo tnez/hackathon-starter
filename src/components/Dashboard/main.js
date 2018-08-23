539f9a79ca374067b3891ed2a85abd5e3fc69394
Travis Nesland <tnesland@gmail.com>
2018-08-23 17:53:38 -0400

    Setup example routes in Dashboard

--- src/components/Dashboard/main.js
+++ src/components/Dashboard/main.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
 import { Anchor, Box, Header, Menu, Sidebar, Split, Title } from 'grommet'
+import { BrowserRouter, Route } from 'react-router-dom'
 
 import { GrommetApp } from '..'
+
+const testComp = (str = 'test component') => () => <div>{str}</div>
+
 
 export default class Dashboard extends React.Component {
   static propTypes = {}
@@ -10,34 +14,36 @@ export default class Dashboard extends React.Component {
 
   render() {
     return (
-      <GrommetApp>
-        <Split flex="right">
-          <Box>
-            <Sidebar colorIndex="neutral-1">
-              <Header pad="medium" justify="between">
-                <Title>Dashboard</Title>
-              </Header>
-              <Box flex="grow" justify="start">
-                <Menu primary={true}>
-                  <Anchor href="#" className="active">
-                    First
-                  </Anchor>
-                  <Anchor href="#">Second</Anchor>
-                  <Anchor href="#">Third</Anchor>
-                </Menu>
-              </Box>
-            </Sidebar>
-          </Box>
-          <Box
-            align="center"
-            colorIndex="neutral-4"
-            full={true}
-            justify="center"
-          >
-            <div>This be teh stuff</div>
-          </Box>
-        </Split>
-      </GrommetApp>
+      <BrowserRouter>
+        <GrommetApp>
+          <Split flex="right">
+            <Box>
+              <Sidebar colorIndex="neutral-1">
+                <Header pad="medium" justify="between">
+                  <Title>Dashboard</Title>
+                </Header>
+                <Box flex="grow" justify="start">
+                  <Menu primary={true}>
+                    <Anchor path={{ path: '/dashboard', index: true }}>A-Thing</Anchor>
+                    <Anchor path="/dashboard/another-thing">Another Thing</Anchor>
+                    <Anchor path="/dashboard/yet-another-thing">Yet Another Thing</Anchor>
+                  </Menu>
+                </Box>
+              </Sidebar>
+            </Box>
+            <Box
+              align="center"
+              colorIndex="neutral-4"
+              full={true}
+              justify="center"
+            >
+              <Route exact path="/dashboard" component={testComp('This be a thing')} />
+              <Route path="/dashboard/another-thing" component={testComp('This be another thing')} />
+              <Route path="/dashboard/yet-another-thing" component={testComp('This be YET another thing')} />
+            </Box>
+          </Split>
+        </GrommetApp>
+      </BrowserRouter>
     )
   }
 }
